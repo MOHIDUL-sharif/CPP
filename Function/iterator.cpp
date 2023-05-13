f34d68ca81b2aca55de5fa025645f5aa545f0fa9
Mohidul Alam <mohidul17@cse.pstu.ac.bd>
2023-05-13 15:16:54 +0600

    u

---
+++ Function/iterator.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string st="abcd";
+    for(auto it=st.rbegin(); it!=st.rend(); it++)
+    {
+        cout<<*it<<endl;
+    }
+    return 0;
+}
