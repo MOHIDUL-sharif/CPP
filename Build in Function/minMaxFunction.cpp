93fedf8d4e60cc7e38062c5395dafc762df4f0c4
Mohidul Alam <mohidul17@cse.pstu.ac.bd>
2023-05-13 13:48:10 +0600

    update

---
+++ Build in Function/minMaxFunction.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=10, b=275;
+    int minima=min(a,b);
+    int maxima=max(a,b);
+    cout<<minima<<" "<<maxima<<endl;
+    return 0;
+}
